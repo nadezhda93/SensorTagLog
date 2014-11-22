dbcd118143d009d06bae667f0f3183589c1262cd
Nadezhda Shivarova <nadia93.ie@gmail.com>
2014-11-22 11:37:20 +0000

    Create new Activity with passed on device name and address.

--- app/src/main/java/com/nadia/android/sensortaglog/BluetoothActivity.java
+++ app/src/main/java/com/nadia/android/sensortaglog/BluetoothActivity.java
@@ -2,12 +2,7 @@ package com.nadia.android.sensortaglog;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothGatt;
-import android.bluetooth.BluetoothGattCallback;
-import android.bluetooth.BluetoothGattCharacteristic;
-import android.bluetooth.BluetoothGattDescriptor;
-import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
-import android.bluetooth.BluetoothProfile;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -24,13 +19,9 @@ import android.widget.ListView;
 import android.widget.Toast;
 import android.os.Handler;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 
 
 public class BluetoothActivity extends Activity {
-    //Device name constant
-    private static final String DEVICE_NAME = "SensorTag";
     private static final String TAG = "BluetoothActivity";
 
     private Button mScanButton;
@@ -91,131 +82,7 @@ public class BluetoothActivity extends Activity {
         }
     }
 
-    //3. gattConnect method to connect to Gatt services on the device that was clicked
-    //in the ListView
-    private void gattConnect(BluetoothDevice device) {
-        //make a connection to the device using LE specific connectGatt() method,
-        //passing in a callback for Gatt events
-        mConnectedGatt = device.connectGatt(this, true, mGattCallback);
-        //start scrolling wheel
-        setProgressBarIndeterminateVisibility(true);
-        mConnectedGatt.discoverServices();
-    }
-
-
-    //4. Gatt service callback methods
-    //Various callback methods defined by the BLE API.
-    private final BluetoothGattCallback mGattCallback =
-            new BluetoothGattCallback() {
-                //method when to connection to GATT server changes
-                @Override
-                public void onConnectionStateChange(BluetoothGatt gatt, int status,
-                                                    int newState) {
-                    if (newState == BluetoothProfile.STATE_CONNECTED) {
-                        Log.d(TAG, "Connected to GATT server.");
-
-                        //Activity.runOnUiThread used to update user interface inside a callback
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                //stop scrolling wheel
-                                setProgressBarIndeterminateVisibility(false);
-                                //show toast to user that they have connected
-                                Toast.makeText(BluetoothActivity.this, "Connected to device", Toast.LENGTH_SHORT).show();
-
-                            }
-                        });
-                        //discover services on device - asynchronous, when successful, calls onServicesDiscovered below
-                        Log.d(TAG, "Attempting to start service discovery:" + gatt.discoverServices());
-                    }
-
-                    else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
-                        Log.d(TAG, "Disconnected from GATT server.");
-                        //Activity.runOnUiThread used to update user interface inside a callback
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                //show toast to user that they have disconnected
-                                Toast.makeText(BluetoothActivity.this, "Disconnected from device", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    }
-                }
-
-                //method when new services are discovered after calling BluetoothGatt.discoverServices();
-                @Override
-                public void onServicesDiscovered(BluetoothGatt gatt, int status) {
-                    if (status == BluetoothGatt.GATT_SUCCESS) {
-                        Log.d(TAG, "Gatt services discovered");
-                        BluetoothGattService humidity = gatt.getService(HUMIDITY_SERVICE);
-                        BluetoothGattCharacteristic humidityData = humidity.getCharacteristic(HUMIDITY_SERVICE_DATA);
-                        Log.d(TAG, String.valueOf(SensorTagDataConvert.extractHumidityValues(humidityData)));
-                    }
-                    else {
-                        Log.d(TAG, "onServicesDiscovered received: " + status);
-                    }
-                }
-
-                // Result of a characteristic read operation
-                @Override
-                public void onCharacteristicRead(BluetoothGatt gatt,
-                                                 BluetoothGattCharacteristic characteristic,
-                                                 int status) {
-                    //DO SOMETHING WHEN SENSOR IS READ
-                }
-
-                //Result of remote characteristic notification
-                @Override
-                public void onCharacteristicChanged(BluetoothGatt gatt,
-                                                    BluetoothGattCharacteristic characteristic){
-                    //DO SOMETHING WHEN NOTIFICATION COMES
-
-                }
-            };
-
-    //5. Get a list of all the services and pass it to the next activity
-    //called inside BluetoothGattCallback because it takes time for
-    //services to be discovered which is asynchronous
-    private void listGattServices(){
-        Log.d(TAG, "listGattServices started");
-
-        List<BluetoothGattService> listOfServices = mConnectedGatt.getServices();
-
-        List<BluetoothGattCharacteristic> characteristics = listOfServices.get(0).getCharacteristics();
-
-        List<BluetoothGattDescriptor> descriptors = characteristics.get(0).getDescriptors();
-
-        if(listOfServices.isEmpty()){
-            Log.d(TAG, "ListOfServices empty");
-        }
-
-        else {
-            Log.d(TAG, "ListOfServices NOT empty");
-
-            for (int i = 0 ; i<characteristics.size(); i++) {
-                //get a list of all characteristics
-                Log.d(TAG, "CHARACTERISTIC: " + characteristics.get(i).toString());
-                //Log.d(TAG, "Service: " + listOfServices.get(i).getUuid().toString());
-                //Log.d(TAG, "Characteristic: "+ c.getService());
-            }
-
-            for (int j = 0; j< descriptors.size(); j++){
-                Log.d (TAG, "DESCRIPTOR: "+ descriptors.get(j).toString());
-            }
-        }
-
-        //Start the new activity to show list of services SensorDataActivity
-        //by making a new Intent and sending on mConnectedGatt
-        //Intent newActivity = new Intent(BluetoothActivity.this, SensorDataActivity.class);
-        //newActivity.putExtra("mConnectedGatt", listOfServices);
-        //startActivity(newActivity);
-    }
-
-
     ////////////////////////////////////////////////////////////////////////////////////
-
-
-
     ////////////////onCreate() Lifecycle Activity Method ///////////////////////////////
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -254,10 +121,15 @@ public class BluetoothActivity extends Activity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 //when clicked, log that it was clicked, connect to gatt server
                 BluetoothDevice clicked_device = mDevicesAdapter.getItem(position);
-
                 Log.d(TAG, clicked_device.getName()+ " was clicked");
-                //connect to gatt on this device
-                gattConnect(clicked_device);
+                // open new activity
+                if (clicked_device == null) {
+                    return;
+                }
+                final Intent intent = new Intent(BluetoothActivity.this, SensorDataActivity.class);
+                intent.putExtra(SensorDataActivity.EXTRAS_DEVICE_NAME, clicked_device.getName());
+                intent.putExtra(SensorDataActivity.EXTRAS_DEVICE_ADDRESS, clicked_device.getAddress());
+                startActivity(intent);
             }
         });
 
--- app/src/main/java/com/nadia/android/sensortaglog/SensorDataActivity.java
+++ app/src/main/java/com/nadia/android/sensortaglog/SensorDataActivity.java
@@ -3,6 +3,7 @@ package com.nadia.android.sensortaglog;
 import android.app.Activity;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGattService;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
@@ -22,82 +23,28 @@ import java.util.UUID;
  * after the user has clicked on a device and has connected to GATT server
  */
 public class SensorDataActivity extends Activity {
-
-    HashMap<UUID, String> mapServices = new HashMap<UUID, String>();
-
-    //1. IR temp UUIDs
-    private static final UUID IR_SERVICE = UUID.fromString("f000aa00-0451-4000-b000-000000000000");
-    private static final UUID IR_SERVICE_DATA = UUID.fromString("f000aa01-0451-4000-b000-000000000000");
-    private static final UUID IR_SERVICE_CONFIG = UUID.fromString("f000aa02-0451-4000-b000-000000000000");
-
-    //2. Accelerometer UUIDs
-    private static final UUID ACCELEROMETER_SERVICE = UUID.fromString("f000aa10-0451-4000-b000-000000000000");
-    private static final UUID ACCELEROMETER_SERVICE_DATA = UUID.fromString("f000aa11-0451-4000-b000-000000000000");
-    private static final UUID ACCELEROMETER_SERVICE_CONFIG = UUID.fromString("f000aa12-0451-4000-b000-000000000000");
-
-    //3. Gyroscope UUIDs
-    private static final UUID GYRO_SERVICE = UUID.fromString("f000aa50-0451-4000-b000-000000000000");
-    private static final UUID GYRO_SERVICE_DATA = UUID.fromString("f000aa51-0451-4000-b000-000000000000");
-    private static final UUID GYRO_SERVICE_CONFIG = UUID.fromString("f000aa52-0451-4000-b000-000000000000");
-
-    //4. Humidity Service UUID
-    private static final UUID HUMIDITY_SERVICE = UUID.fromString("f000aa20-0451-4000-b000-000000000000");
-    private static final UUID HUMIDITY_SERVICE_DATA = UUID.fromString("f000aa21-0451-4000-b000-000000000000");
-    private static final UUID HUMIDITY_SERVICE_CONFIG = UUID.fromString("f000aa22-0451-4000-b000-000000000000");
-
-    //5. Magnetometer UUIDs
-    private static final UUID MAGNETOMETER_SERVICE = UUID.fromString("f000aa30-0451-4000-b000-000000000000");
-    private static final UUID MAGNETOMETER_DATA = UUID.fromString("f000aa31-0451-4000-b000-000000000000");
-    private static final UUID MAGNETOMETER_CONFIG = UUID.fromString("f000aa32-0451-4000-b000-000000000000");
-
-    //6. Barometer UUIDs
-    private static final UUID BAROMETER_SERVICE = UUID.fromString("f000aa40-0451-4000-b000-000000000000");
-    private static final UUID BAROMETER_DATA = UUID.fromString("f000aa41-0451-4000-b000-000000000000");
-    private static final UUID BAROMETER_CONFIG = UUID.fromString("f000aa42-0451-4000-b000-000000000000");
-
-
-
-
     private static final String TAG = "SensorDataActivity";
-    private ListView mSensorsListView;
-    private ArrayAdapter<String> mSensorsAdapter;
-    private ArrayList<String> mListOfSensors = new ArrayList<String>();
+    public static final String EXTRAS_DEVICE_NAME = "ExtrasDeviceName";
+    public static final String EXTRAS_DEVICE_ADDRESS = "ExtrasDeviceAddress";
 
+    private String mDeviceName;
+    private String mDeviceAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i(TAG, "onCreate started");
         setContentView(R.layout.activity_sensor_data);
+        // extract intent information
+        final Intent intent = getIntent();
+        mDeviceName = intent.getStringExtra(EXTRAS_DEVICE_NAME);
+        mDeviceAddress = intent.getStringExtra(EXTRAS_DEVICE_ADDRESS);
+        Log.d(TAG, "Extracted device name: " + mDeviceName);
+        Log.d(TAG, "Extracted device address: " + mDeviceAddress);
+    }
 
-        //wire up widgets
-        mSensorsListView = (ListView) findViewById(R.id.sensors_found_list);
-
-        //set the adapter to the listView in the layout using default android layout
-        mSensorsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mListOfSensors);
-        mSensorsListView.setAdapter(mSensorsAdapter);
-
-
-        //respond to clicks in the list view
-        mSensorsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                //DO SOMETHING ON CLICK
-
-            }
-        });
-
-        mapServices.put(IR_SERVICE, "Infrared Temperature");
-        mapServices.put(HUMIDITY_SERVICE, "Humidity");
-        mapServices.put(BAROMETER_SERVICE, "Barometer");
-
-        mListOfSensors.add(mapServices.get(IR_SERVICE));
-        mListOfSensors.add(mapServices.get(HUMIDITY_SERVICE));
-        mListOfSensors.add(mapServices.get(BAROMETER_SERVICE));
-
-        mSensorsAdapter.notifyDataSetChanged();
-
-
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
